64ee79e6cc6d6f9f0356f0a96f89853db4d322a4
Owen Chang <owenchang.dev@gmail.com>
2022-08-04 19:45:31 +0800

    feature: fix typing error

--- src/features/about/About.tsx
+++ src/features/about/About.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => (
   <section id="about">
     <h5>Get To Know</h5>
-    <h2>About me</h2>
+    <h2>About Me</h2>
 
     <div className="container about__container">
       <div className="about__me">
